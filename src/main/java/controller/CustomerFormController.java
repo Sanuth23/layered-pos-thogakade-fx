88b79d3384f0cda71233be913f646e0e403b5c94
Sanuth23 <sanuth0000@gmail.com>
2023-12-22 15:08:28 +0530

    Customer Bo update;

--- src/main/java/controller/CustomerFormController.java
+++ src/main/java/controller/CustomerFormController.java
@@ -67,7 +67,7 @@ public class CustomerFormController {
     @FXML
     private TreeTableColumn<?, ?> colOption;
 
-    private CustomerBo<CustomerDto> customerBo = new CustomerBoImpl();
+    private CustomerBo customerBo = new CustomerBoImpl();
 
     public void initialize(){
         colId.setCellValueFactory(new TreeItemPropertyValueFactory<>("id"));
